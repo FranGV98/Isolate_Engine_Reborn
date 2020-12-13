5a56e2e26120c4dc1cb8e4bb15fa4b4c50c420be
Turmo11 <Turmo11@users.noreply.github.com>
2020-12-13 23:22:44 +0100

    Erased unnecessary information

--- Isolate_Engine_Reborn/Source/I_Meshes.cpp
+++ Isolate_Engine_Reborn/Source/I_Meshes.cpp
@@ -1,7 +1,3 @@
-// ----------------------------------------------------
-// I_Meshes.cpp --- Importing .FBX files into meshes.
-// Interface class between the engine and Assimp.
-// ----------------------------------------------------
 
 #include "Assimp.h"
 
@@ -16,7 +12,6 @@
 
 #pragma comment (lib, "Source/Dependencies/Assimp/libx86/assimp.lib")
 
-using namespace Importer::Meshes;																	// Not a good thing to do but it will be employed sparsely and only inside this .cpp
 
 uint64 Importer::Meshes::Save(const R_Mesh* mesh, const char* path, char** buffer)
 {
@@ -29,9 +24,9 @@ uint64 Importer::Meshes::Save(const R_Mesh* mesh, const char* path, char** buffe
 		mesh->normals.size(),
 		mesh->tex_coords.size(),
 		mesh->indices.size(),
-		mesh->VBO, 																				// 4 --> Vertices Buffer Object ID
-		mesh->NBO, 																				// 5 --> Normals Buffer Object ID
-		mesh->TBO, 																				// 6 --> Texture Coordinates Buffer Object ID
+		mesh->VBO, 																				
+		mesh->NBO, 																				
+		mesh->TBO, 																				
 		mesh->IBO
 	};
 	uint size = sizeof(header) + sizeof(uint);
@@ -142,8 +137,6 @@ bool Importer::Meshes::Load(const char* buffer, R_Mesh* mesh)
 
 void Importer::Meshes::Import(const char* path, std::vector<R_Mesh*>& meshes)
 {
-	//const aiScene* scene = aiImportFile(path, aiProcessPreset_TargetRealtime_MaxQuality);
-
 	char* buffer = nullptr;
 	uint file_size = App->file_system->Load(path, &buffer);
 
@@ -162,22 +155,22 @@ void Importer::Meshes::Import(const char* path, std::vector<R_Mesh*>& meshes)
 	}
 }
 
-void Utilities::ProcessNode(const aiScene* scene, aiNode* node, std::vector<R_Mesh*>& meshes)		// Shortened with the use of used namespaces, otherwise it would be a bad idea.
+void Importer::Meshes::Utilities::ProcessNode(const aiScene* scene, aiNode* node, std::vector<R_Mesh*>& meshes)
 {
-	for (uint i = 0; i < node->mNumMeshes; ++i)														// The loop is run for as many meshes that the node has registered it has.
+	for (uint i = 0; i < node->mNumMeshes; ++i)														
 	{
-		aiMesh* ai_mesh = scene->mMeshes[node->mMeshes[i]];											// Gets the mesh specifiec by the mesh index stored in the node's mMeshes array.
+		aiMesh* ai_mesh = scene->mMeshes[node->mMeshes[i]];											
 		
-		if (ai_mesh != nullptr && ai_mesh->HasFaces())												// Checks that the aiMesh is valid.
+		if (ai_mesh != nullptr && ai_mesh->HasFaces())												
 		{
-			R_Mesh* r_mesh = new R_Mesh();													// Generates a new R_Mesh.
-			r_mesh->og_trans = new Transform();															// Generates a new R_Mesh.
+			R_Mesh* r_mesh = new R_Mesh();													
+			r_mesh->og_trans = new Transform();														
 
-			Utilities::GenerateMesh(scene, ai_mesh, r_mesh);										// Sets the given r_mesh with the data stored in ai_mesh.
-			Utilities::GenerateTransform(scene, node, r_mesh->og_trans);
-			if (r_mesh != nullptr)																	// Checks that the R_Mesh* is valid/stores data.
+			Importer::Meshes::Utilities::GenerateMesh(scene, ai_mesh, r_mesh);						
+			Importer::Meshes::Utilities::GenerateTransform(scene, node, r_mesh->og_trans);
+			if (r_mesh != nullptr)																	
 			{
-				meshes.push_back(r_mesh);															// Adds the R_Mesh* to the given meshes vector.
+				meshes.push_back(r_mesh);															
 			}
 			else
 			{
@@ -195,32 +188,32 @@ void Utilities::ProcessNode(const aiScene* scene, aiNode* node, std::vector<R_Me
 
 	for (uint i = 0; i < node->mNumChildren; ++i)
 	{
-		Utilities::ProcessNode(scene, node->mChildren[i], meshes);
+		Importer::Meshes::Utilities::ProcessNode(scene, node->mChildren[i], meshes);
 	}
 }
 
 void Importer::Meshes::Utilities::GenerateMesh(const aiScene* ai_scene, const aiMesh* ai_mesh, R_Mesh* r_mesh)
 {
 	// Allocating the required memory for each vector
-	uint vertices_size = ai_mesh->mNumVertices * 3;											// There will be 3 coordinates per vertex, hence the size will be numVertices * 3.
-	r_mesh->vertices.resize(vertices_size);													// Allocating in advance the required memory to store all the verts.
+	uint vertices_size = ai_mesh->mNumVertices * 3;			
+	r_mesh->vertices.resize(vertices_size);					
 
-	uint normals_size = ai_mesh->mNumVertices * 3;											// There will be 3 normal coordinates per vertex.
+	uint normals_size = ai_mesh->mNumVertices * 3;			
 	r_mesh->normals.resize(normals_size);
 
-	uint tex_coords_size = ai_mesh->mNumVertices * 2;										// There will be 2 tex coordinates per vertex.
+	uint tex_coords_size = ai_mesh->mNumVertices * 2;		
 	r_mesh->tex_coords.resize(tex_coords_size);
 
-	uint indices_size = ai_mesh->mNumFaces * 3;												// The size of the indices vector will be equal to the amount of faces times 3 (triangles).
-	r_mesh->indices.resize(indices_size);													// Allocating in advance the required memory to store all the indices.
+	uint indices_size = ai_mesh->mNumFaces * 3;				
+	r_mesh->indices.resize(indices_size);					
 
 	// Loading the data from the mesh into the corresponding vectors
-	Utilities::GetVertices(ai_mesh, r_mesh, vertices_size);									// 
-	Utilities::GetNormals(ai_mesh, r_mesh, normals_size);									// 
-	Utilities::GetTexCoords(ai_mesh, r_mesh, tex_coords_size);								// 
-	Utilities::GetIndices(ai_mesh, r_mesh, indices_size);									// 
+	Importer::Meshes::Utilities::GetVertices(ai_mesh, r_mesh, vertices_size);								
+	Importer::Meshes::Utilities::GetNormals(ai_mesh, r_mesh, normals_size);									
+	Importer::Meshes::Utilities::GetTexCoords(ai_mesh, r_mesh, tex_coords_size);							
+	Importer::Meshes::Utilities::GetIndices(ai_mesh, r_mesh, indices_size);									
 
-	Utilities::GetTexturePaths(ai_scene, ai_mesh, r_mesh);									// Will get the filename associated with ai_mesh and store it inside r_mesh's tex path vector.
+	Importer::Meshes::Utilities::GetTexturePaths(ai_scene, ai_mesh, r_mesh);								
 
 	r_mesh->LoadBuffers();
 
@@ -273,7 +266,7 @@ void Importer::Meshes::Utilities::GetVertices(const aiMesh* ai_mesh, R_Mesh* r_m
 {
 	if (ai_mesh->HasPositions())
 	{
-		memcpy(&r_mesh->vertices[0], ai_mesh->mVertices, sizeof(float) * size);	// &r_mesh->vertices[0] gets a pointer to the beginning of the vector. mVertices is a 3D vector.
+		memcpy(&r_mesh->vertices[0], ai_mesh->mVertices, sizeof(float) * size);	
 
 		LOG("[STATUS] Imported %u position vertices!", size)
 	}
@@ -317,17 +310,15 @@ void Importer::Meshes::Utilities::GetTexCoords(const aiMesh* ai_mesh, R_Mesh* r_
 
 void Importer::Meshes::Utilities::GetIndices(const aiMesh* ai_mesh, R_Mesh* r_mesh, uint size)
 {
-	if (ai_mesh->HasFaces())																	// Double checked if for whatever reason Generate Mesh is called independently.
+	if (ai_mesh->HasFaces())																
 	{
 		uint ind = 0;
 		for (uint i = 0; i < ai_mesh->mNumFaces; ++i)
 		{
-			aiFace face = ai_mesh->mFaces[i];													// Getting the face that is currently being iterated.
+			aiFace face = ai_mesh->mFaces[i];												
 
 			if (face.mNumIndices == 3)
 			{
-				//memcpy(&r_mesh->indices[i * 3], face.mIndices, sizeof(uint) * 3);
-
 				for (uint j = 0; j < face.mNumIndices; ++j)
 				{
 					r_mesh->indices[ind] = face.mIndices[j];
@@ -372,9 +363,6 @@ void Importer::Meshes::Utilities::GetTexturePaths(const aiScene* ai_scene, const
 
 				r_mesh->tex_paths.push_back(file_path.c_str());
 			}
-
-
-			//r_mesh->tex_paths.push_back(path.C_Str());
 
 			LOG("[STATUS] Mesh texture file name %u is %s", i, path.C_Str());
 		}
