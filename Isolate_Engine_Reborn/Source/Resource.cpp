d4038a226f5cef7e2fb4aa1d719cb0e34f84dd57
Turmo11 <Turmo11@users.noreply.github.com>
2020-12-13 17:29:36 +0100

    Added more functionalities to the resources

--- Isolate_Engine_Reborn/Source/Resource.cpp
+++ Isolate_Engine_Reborn/Source/Resource.cpp
@@ -3,8 +3,8 @@
 #include "Randomizer.h"
 
 Resource::Resource(RESOURCE_TYPE type) : 
-type	(type), 
-uid		(Randomizer::GetBoundedRandomUint()) //Generate UID
+type (type), 
+uid	 (Randomizer::GetBoundedRandomUint()) //Generate UID
 {
 
 }
@@ -14,7 +14,46 @@ Resource::~Resource()
 
 }
 
+void Resource::SetUID(uint32 new_UID)
+{
+	uid = new_UID;
+}
 void Resource::GenerateNewUID()
 {
 	uid = Randomizer::GetRandomUint();
+}
+
+void Resource::SetAssetsPath(const char* new_path)
+{
+	assets_path = new_path;
+}
+
+void Resource::SetLibraryPath()
+{
+	std::string directory;
+	std::string name = std::to_string(uid);
+	std::string extension;
+
+	switch (type)
+	{
+	case RESOURCE_TYPE::MESH:
+		directory = MESHES_PATH;
+		extension = CUSTOM_MESH_EXT;
+		break;
+
+	case RESOURCE_TYPE::MATERIAL:
+		directory = MATERIALS_PATH;
+		extension = CUSTOM_MAT_EXT;
+		break;
+
+	case RESOURCE_TYPE::TEXTURE:
+		directory = TEXTURES_PATH;
+		extension = CUSTOM_TEX_EXT;
+		break;
+	}
+	library_path = directory + name + extension;
+
+	directory.clear();
+	name.clear();
+	extension.clear();
 }
--- Isolate_Engine_Reborn/Source/Resource.h
+++ Isolate_Engine_Reborn/Source/Resource.h
@@ -9,10 +9,16 @@
 
 enum class RESOURCE_TYPE
 {
-	UNKNOWN,
+	NONE,
 	MESH,
 	MATERIAL,
-	TEXTURE
+	TEXTURE,
+	UNKNOWN
+};
+
+struct ResourceData
+{
+
 };
 
 class Resource
@@ -23,17 +29,25 @@ public:
 
 	const char* GetName() const { return name; }
 	uint32 GetUID() const { return uid; }
+	RESOURCE_TYPE GetType() const { return type; }
+	const char* GetAssetsPath() { return assets_path.c_str(); }
+	const char* GetLibraryPath() { return library_path.c_str(); }
 
+	void SetUID(uint32 new_UID);
 	void GenerateNewUID();
+
+	void SetAssetsPath(const char* new_path);
+	void SetLibraryPath();
 
 private:
 
 	RESOURCE_TYPE	type;
 
-	const char*		name;
+	const char* name;
 	uint32			uid;
 
-	std::string		path;
+	std::string		assets_path;
+	std::string		library_path;
 };
 
 #endif // !__RESOURCE_H__
